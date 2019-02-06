d7d8555df9039bbe81fb7c9a6d97d04d7b74b4ff
jpclark6 <jpclark6@gmail.com>
2019-02-06 09:29:23 -0700

    feature test to see data written

--- spec/features/user_sees_energy_info_spec.rb
+++ spec/features/user_sees_energy_info_spec.rb
@@ -3,15 +3,25 @@ require 'rails_helper'
 describe 'as a user' do
   it 'can see electric and propane name, address, fuel type, distance and access times' do
     visit '/'
-    
+    fill_in :q, with: '80203'
+    click_button 'Locate'
+    expect(current_path).to eq('/search')
+    expect(all('.station').length).to eq(10)
+    within(all('.station').first) do
+      expect(page).to have_content('Name: ')
+      expect(page).to have_content('Address: ')
+      expect(page).to have_content('Fuel Types: ')
+      expect(page).to have_content('Distance: ')
+      expect(page).to have_content('Access Times: ')
+    end
   end
 end
 
-As a user
-When I visit "/"
-And I fill in the search form with 80203 (Note: Use the existing search form)
-And I click "Locate"
-Then I should be on page "/search"
-Then I should see a list of the 10 closest stations within 6 miles sorted by distance
-And the stations should be limited to Electric and Propane
-And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
+# As a user
+# When I visit "/"
+# And I fill in the search form with 80203 (Note: Use the existing search form)
+# And I click "Locate"
+# Then I should be on page "/search"
+# Then I should see a list of the 10 closest stations within 6 miles sorted by distance
+# And the stations should be limited to Electric and Propane
+# And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
