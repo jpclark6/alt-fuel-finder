2a0d2bd78c03fad1a9b96605119a7c9934bfad8b
jpclark6 <jpclark6@gmail.com>
2019-02-06 10:32:35 -0700

    facade tests written

---
+++ spec/facades/energy_facade_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe 'as a facade' do
+  it 'exists' do
+    zip = 80203
+    facade = EnergyFacade.new(zip)
+    expect(facade.class).to eq(EnergyFacade)
+  end
+
+  it 'can build addresses correctly' do
+    zip = 80203
+    facade = EnergyFacade.new(zip)
+    service = EnergyService.new(zip).stations
+    station = service[:fuel_stations].first
+    expect(facade.build_address(station)).to eq('800 Acoma St, Denver, CO')
+  end
+
+  it 'can find real name for fuel' do
+    zip = 80203
+    facade = EnergyFacade.new(zip)
+    service = EnergyService.new(zip).stations
+    station = service[:fuel_stations].first
+    expect(facade.find_fuel(station)).to eq('Electric')
+  end
+
+  it 'limits search results to 10 stations' do
+    zip = 80203
+    facade = EnergyFacade.new(zip)
+    expect(facade.stations.length).to eq(10)
+  end
+end
